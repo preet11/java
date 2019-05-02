dce94be0fff614ec36202c363e0682122dfb725b
Gurpreet <Gurpreet@Gurpreet-PC>
2019-05-02 22:28:55 +0530

    Methods

---
+++ Methods/src/MethodFrames.java
@@ -0,0 +1,24 @@
+
+public class MethodFrames {
+	
+	static void addNumbers(int x, int y){
+		int z = x + y;
+		System.out.println("add numbers Sum is: "+z);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("Sum is: "+c);
+		
+		MethodFrames.addNumbers(50, 80);
+				
+		System.out.println("This is Last Statement");
+
+		
+	}
+
+}
